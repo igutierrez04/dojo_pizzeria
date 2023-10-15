8e2646aa4f213fe763e2a8b652617817a436a023
Ivan <igutierrez0794@gmail.com>
2023-10-15 16:46:15 -0400

    finished bonus challenge. assignment complete.

--- dojo_pizzeria.js
+++ dojo_pizzeria.js
@@ -18,3 +18,68 @@ console.log(margheritaPizza);
 
 var tacoPizza = pizzaOven("traditional", "refried bean spread", ["cheddar cheese"], ["ranch dressing", "tortilla chips", "romain lettuce", "tomatoes", "onions", "black olives", "hot sauce"]);
 console.log(tacoPizza);
+
+var crustTypes = [
+    "traditional",
+    "thin crust",
+    "deep dish",
+    "hand tossed",
+    "cauliflower"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce"
+];
+
+var cheeses = [
+    "cheedar",
+    "mozzarella",
+    "feta",
+    "pepper jack",
+    "burrata",
+    "goat cheese"
+];
+
+var toppings = [
+    "tomato",
+    "onions",
+    "black olives",
+    "basil",
+    "chicken",
+    "ham",
+    "pineapple",
+    "mushrooms",
+    "pepperoni",
+];
+
+function randomRange(max, min){
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function pickRandom(arr){
+    var pick = Math.floor(arr.length * Math.random());
+    return arr[pick];
+}
+
+function randomPizza(){
+    pizza = {};
+    pizza.crustType = pickRandom(crustTypes);
+    pizza.sauceType = pickRandom(sauceTypes);
+    pizza.cheese = [];
+    pizza.toppings = [];
+    // for loop to pick a range of different cheeses
+    for(var i=0; i<randomRange(5,1); i++){
+        pizza.cheese.push(pickRandom(cheeses));
+    }
+    // for loop to pick a range of different toppings
+    for(var i=0; i<randomRange(5,0); i++){
+        pizza.toppings.push(pickRandom(toppings));
+    }
+    return pizza;
+}
+
+var randomPizza = randomPizza();
+console.log(randomPizza);
